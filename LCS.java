28380aab35df1d061295b6f1184defb40123b005
lindseysomwaru <lindsey.somwaru@gmail.com>
2022-12-05 11:53:00 -0500

    User Input Added

--- LCS.java
+++ LCS.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 public class LCS {
     public static void lcs(String s1, String s2, int length) {
+        
+        //stores solutions to previously computed subproblems
         int[][] table = new int[length+1][length+1];
+
+        //fill table in bottom-up approach
         for(int i=0; i <= length; i++)
             for(int j=0; j <= length; j++) {
-                if( i == 0 || j == 0)
+                if(i == 0 || j == 0)
                     table[i][j] = 0;
+                //if current character of 2 strings match
                 else if(s1.charAt(i-1) == s2.charAt(j-1))
                     table[i][j] = table[i-1][j-1] + 1;
                 else
                     table[i][j] = Math.max(table[i-1][j], table[i][j-1]);
             }
+        
         int index = table[length][length];
         int tmp = index;
         char[] lcs = new char[index+1];
@@ -28,6 +34,7 @@ public class LCS {
             else
                 j--;
         }
+        //printing LCS and length of LCS
         if(lcs[0] == 0)
             System.out.print("Sequence 1: " + s1 + "\nSequence 2: " + s2 + "\nLCS: none" + "\nLength of LCS: 0");
         else {
@@ -46,10 +53,13 @@ public class LCS {
         Scanner scanner = new Scanner(System.in);
         String s1, s2;
         System.out.print("Enter a string of length 6: ");
-        s1 = scanner.nextLine();
+        s1 = scanner.nextLine().toUpperCase();
         System.out.print("Enter a string of length 6: ");
-        s2 = scanner.nextLine();
+        s2 = scanner.nextLine().toUpperCase();
         scanner.close();
-        lcs(s1,s2,SEQUENCE_LENGTH);
+        if(s1.length() != 6 || s2.length() != 6)
+            System.out.println("STRING MUST BE LENGTH 6! TRY AGAIN!");
+        else
+            lcs(s1,s2,SEQUENCE_LENGTH);
     }
 }
---
+++ readme.md
@@ -0,0 +1,6 @@
+# <div align="center">**Project 3**</div>
+## **Overview**
+The following program finds the longest common subsequence given two String sequences of length 6. The LCS algorithm is implemented using a bottom-up approach. The program uses one void helper method, *lcs(String s1, String s2, int length)*, and a main method.
+
+## **Running the File**
+The user will first be prompted to enter two strings of length six. Only strings with length six can will be accepted; if a string of any other length is entered, an error message will appear, and the program will terminate. Once the user enters two valid strings, the program will determine the LCS and its length. The two original strings, the LCS, and the length of the LCS will be printed.
